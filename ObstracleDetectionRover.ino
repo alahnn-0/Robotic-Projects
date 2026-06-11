6fd85631141c40f56a097b9aedf6d5739a039cf7
Alan Baby <alanbaby2744@gmail.com>
2026-06-11 21:53:04 +0530

    Update obstacle detection and motor control functions
    Refactor obstacle detection logic and motor control functions.

--- ObstracleDetectionRover.ino
+++ ObstracleDetectionRover.ino
@@ -1,36 +1,36 @@
-#define TRIG_PIN 9
-#define ECHO_PIN 10
+#include <Servo.h>
+
+// Motor Driver Pins
 #define IN1 2
 #define IN2 3
 #define IN3 4
 #define IN4 5
 
-void setup() {
-  Serial.begin(9600);
+// Ultrasonic Sensor Pins
+#define ECHO_PIN 9
+#define TRIG_PIN 10
 
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+// Servo Pin
+#define SERVO_PIN 11
 
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-}
+Servo scanner;
+
+// ---------- Motor Functions ----------
 
 void moveForward() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-}
-
-void moveBackward() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
 
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
+}
+
+void moveBackward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
 void stopMotors() {
@@ -42,6 +42,14 @@ void stopMotors() {
 }
 
 void turnLeft() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void turnRight() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
 
@@ -49,51 +57,134 @@ void turnLeft() {
   digitalWrite(IN4, LOW);
 }
 
-bool checkDistance(float distance) {
-  if (distance < 30) {
-    return true;
-  }
-  return false;
-}
+// ---------- Ultrasonic Function ----------
 
-void loop() {
-
-  long duration;
-  float distance;
-
+float getDistance() {
 
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
 
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
+
   digitalWrite(TRIG_PIN, LOW);
 
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distance = duration * 0.0343 / 2;
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000);
 
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  if (duration == 0)
+    return 400;
 
-  if (checkDistance(distance)) {
+  return duration * 0.0343 / 2;
+}
+
+// ---------- Setup ----------
+
+void setup() {
+
+  Serial.begin(9600);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  scanner.attach(SERVO_PIN);
+  scanner.write(90); // Center
+
+  delay(1000);
+}
+
+// ---------- Main Loop ----------
+
+void loop() {
+
+  // Look forward
+  scanner.write(90);
+  delay(200);
+
+  float frontDistance = getDistance();
+
+  Serial.print("Front: ");
+  Serial.println(frontDistance);
+
+  // No obstacle
+  if (frontDistance > 25) {
+
+    moveForward();
+  }
+
+  // Obstacle detected
+  else {
+
+    Serial.println("Obstacle Detected");
 
     stopMotors();
     delay(200);
 
+    // Move backward a little
     moveBackward();
     delay(500);
 
     stopMotors();
     delay(200);
 
-    turnLeft();
-    delay(700);
+    // Check RIGHT first
+    scanner.write(180);
+    delay(600);
+
+    float rightDistance = getDistance();
+
+    Serial.print("Right: ");
+    Serial.println(rightDistance);
+
+    if (rightDistance > 25) {
+
+      Serial.println("Turning Right");
+
+      turnRight();
+      delay(700);
+
+      stopMotors();
+
+      scanner.write(90);
+      delay(200);
+
+      return;
+    }
+
+    // Check LEFT
+    scanner.write(0);
+    delay(600);
+
+    float leftDistance = getDistance();
+
+    Serial.print("Left: ");
+    Serial.println(leftDistance);
+
+    if (leftDistance > 25) {
+
+      Serial.println("Turning Left");
+
+      turnLeft();
+      delay(700);
+
+      stopMotors();
+
+      scanner.write(90);
+      delay(200);
+
+      return;
+    }
+
+    // Both sides blocked
+    Serial.println("No Path Available");
 
     stopMotors();
-  }
-  else {
-    moveForward();
+
+    scanner.write(90);
   }
 
   delay(100);
